e25e771cc3af3650290eba65116199a97dbba44b
mark <mark.mekhayl@gmail.com>
2025-05-06 16:10:21 +1000

    fixed it, i think for real now

--- src/utils/getAssetPath.js
+++ src/utils/getAssetPath.js
@@ -1,9 +1,7 @@
 export function getAssetPath(relativePath) {
   const isLocal = window.location.hostname === "localhost" || window.location.hostname.startsWith('127.0.0');
 
-  const basePath = isLocal
-    ? "/public/assets/"
-    : "/public/assets/";
+  const basePath = '/assets/'
 
   return `${basePath}${relativePath}`;
 }
